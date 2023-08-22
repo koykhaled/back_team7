b54c90d7016a71867868035848c869518c5de2ee
rawandkh <rawandkhamis@gmail.com>
2023-08-22 01:36:20 -0700

    add Terrm crud

--- app/Http/Controllers/Api/UploadImage.php
+++ app/Http/Controllers/Api/UploadImage.php
@@ -8,11 +8,18 @@ trait UploadImage
     {
         try {
             $dir = 'images/' . $name;
-            if ($file = $request->file($input)) {
-                $image_ext = $file->getClientOriginalExtenion();
-                $image_name = $data->uuid;
-                $image_full_name = $image_name . "." . $image_ext;
-                $file->move($dir, $image_full_name);
+            if ($fileData = $request->input($input)) {
+                // Extract the image extension from the base64 data
+                $extension = explode('/', mime_content_type($fileData))[1];
+                $image_name = 'data';
+                $image_full_name = $image_name . "." . $extension;
+
+                // Decode the base64 image data
+                $file = base64_decode($fileData);
+
+                // Save the image file
+                file_put_contents($dir . $image_full_name, $file);
+
                 $data->updateOrCreate(
                     ['uuid' => $data->uuid],
                     [$input => $dir . $image_full_name]
